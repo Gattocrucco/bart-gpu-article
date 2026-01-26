b11190c57ef8c47a30f495e2f81d46ec6a0b4753
Gattocrucco <info@giacomopetrillo.com>
2026-01-26 15:54:06 +0000

    perf: optimize data generation for gpu

--- src/bart_gpu_article/scripts/benchmark.py
+++ src/bart_gpu_article/scripts/benchmark.py
@@ -2,9 +2,11 @@
 
 import json
 import math
+import sys
 from abc import ABC, abstractmethod
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
+from dataclasses import replace
 from functools import partial
 from gc import collect
 from pathlib import Path
@@ -12,7 +14,7 @@ from time import perf_counter
 from typing import Any, Literal
 
 from bartz import mcmcloop
-from bartz.jaxext import split
+from bartz.jaxext import autobatch, split
 from bartz.mcmcstep import State, init, make_p_nonterminal
 from bartz.prepcovars import (
     bin_predictors,
@@ -32,7 +34,7 @@ from jax import (
 )
 from jax import numpy as jnp
 from jax.errors import JaxRuntimeError
-from jaxtyping import Array, Float, Float32, Key, UInt
+from jaxtyping import Array, Float, Float32, Key, UInt, UInt8
 
 from bart_gpu_article.dgp import gen_data
 
@@ -49,7 +51,9 @@ class UnitConfig(Module):
     cpu_max_memory: int
     xgboost_gpu_max_n_times_p: int
     device: Device
+    data_device: Device
     benchclass: type["Benchmark"]
+    quantize_x: bool
 
 
 class Config(Module):
@@ -70,8 +74,17 @@ class Config(Module):
     seed: int = 2026_01_24_16_54
 
     def device(self) -> Device:
-        """Get the jax device to use."""
+        """Get the jax device to use for running the algorithm."""
         return devices(self.platform)[0]
+
+    def data_device(self) -> Device:
+        """Get the jax device to use for data generation."""
+        platform = "cpu" if self.benchlabel == "xgboost" else self.platform
+        return devices(platform)[0]
+
+    def quantize_x(self) -> bool:
+        """Determine whether X should be quantized or original."""
+        return self.benchlabel == "bartz"
 
     def unit_config(self, n: int) -> UnitConfig:
         """Return the specific config at sample size `n`."""
@@ -87,15 +100,17 @@ class Config(Module):
             cpu_max_memory=self.cpu_max_memory,
             xgboost_gpu_max_n_times_p=self.xgboost_gpu_max_n_times_p,
             device=self.device(),
+            data_device=self.data_device(),
             benchclass=Benchmark.subclasses[self.benchlabel],
+            quantize_x=self.quantize_x(),
         )
 
 
 class Data(Module):
     """Simulated data."""
 
-    raw_X: Float[Array, "p n"]
-    quantized_X: UInt[Array, "p n"]
+    raw_X: Float[Array, "p n"] | None
+    quantized_X: UInt[Array, "p n"] | None
     y: Float32[Array, " n"]
     max_split: UInt[Array, " p"]
     prior_var: Float32[Array, ""]
@@ -103,13 +118,36 @@ class Data(Module):
     eps_var: Float32[Array, ""]
 
 
-@partial(jit, static_argnums=(1, 2))
-def make_data(key: Key[Array, ""], n: int, p: int) -> Data:
+@partial(jit, static_argnums=(1, 2, 3))
+def make_data(key: Key[Array, ""], n: int, p: int, quantized_x: bool) -> Data:
     """Generate data."""
-    # generate data
+
+    @partial(
+        autobatch,
+        out_axes=Data(1, 1, 0, 0, 0, 0, 0),
+        max_io_nbytes=2**26,
+    )
+    def make_data_batchable(keys: Key[Array, " n"]):
+        return _make_data_batchable(keys[0], keys.size, p, quantized_x)
+
+    unbatched_data = _make_data_unbatchable(key, p)
+    batched_data = make_data_batchable(random.split(key, n))
+
+    return Data(
+        raw_X=batched_data.raw_X,
+        quantized_X=batched_data.quantized_X,
+        y=batched_data.y,
+        max_split=unbatched_data.max_split,
+        prior_var=unbatched_data.prior_var,
+        pop_var=unbatched_data.pop_var,
+        eps_var=unbatched_data.eps_var,
+    )
+
+
+def _gen_data_kwargs(n: int, p: int) -> dict[str, Any]:
+    """Generate arguments for `gen_data`."""
     sigma2 = 1 / 3
-    data = gen_data(
-        key,
+    return dict(
         n=n,
         p=p,
         q=2 if p > 2 else 0,
@@ -118,19 +156,52 @@ def make_data(key: Key[Array, ""], n: int, p: int) -> Data:
         sigma2_eps=sigma2,
     )
 
+
+def _make_data_unbatchable(key: Key[Array, ""], p: int) -> Data:
+    """Produce those parts of data that do not have 'n' amongst axis dims."""
+    max_split = jnp.full((p,), 255, jnp.uint8)
+    kwargs = _gen_data_kwargs(0, p)
+    empty_data = gen_data(key, **kwargs)
+    return Data(
+        raw_X=None,
+        quantized_X=None,
+        y=None,
+        max_split=max_split,
+        prior_var=empty_data.sigma2_pri,
+        pop_var=empty_data.sigma2_pop,
+        eps_var=empty_data.sigma2_eps,
+    )
+
+
+def _make_data_batchable(
+    key: Key[Array, ""], n: int, p: int, quantized_x: bool
+) -> Data:
+    """Internal implementation of make_data."""
+    # generate data
+    kwargs = _gen_data_kwargs(n, p)
+    data = gen_data(key, **kwargs)
+
     # quantize predictors
-    splits, max_split = uniform_splits_from_matrix(data.x, 256)
-    X = bin_predictors(data.x, splits)
+    if quantized_x:
+        qx = _quantize_uniform(data.x)
 
     return Data(
-        raw_X=data.x,
-        quantized_X=X,
+        raw_X=() if quantized_x else data.x,
+        quantized_X=qx if quantized_x else (),
         y=data.y,
-        max_split=max_split,
-        prior_var=data.sigma2_pri,
-        pop_var=data.sigma2_pop,
-        eps_var=data.sigma2_eps,
+        max_split=(),
+        prior_var=(),
+        pop_var=(),
+        eps_var=(),
     )
+
+
+def _quantize_uniform(x: Float[Array, "p n"]) -> UInt8[Array, "p n"]:
+    l = -(3**0.5)
+    u = 3**0.5
+    # see dgp.py for the bounds
+    qx = jnp.floor((x - l) / (u - l) * 256.0)
+    return qx.astype(jnp.uint8)
 
 
 def num2si(
@@ -166,8 +237,8 @@ def format_mem(m: float) -> str:
     return f"{num2si(m)}B"
 
 
-class Skip(Exception):
-    """Exception to be raised to skip a benchmark unit."""
+class Stop(Exception):
+    """Exception to be raised to stop the benchmarking loop."""
 
 
 class Benchmark(ABC):
@@ -203,32 +274,29 @@ class Bartz(Benchmark):
         expected_memory_usage = cfg.n * (cfg.ntree + cfg.p)
         if cfg.device.platform == "cpu" and expected_memory_usage > cfg.cpu_max_memory:
             # on cpu, jax won't raise out of memory errors, and just hang forever
-            raise Skip(
+            raise Stop(
                 f"cpu memory limit exceeded: {format_mem(expected_memory_usage)} > {format_mem(cfg.cpu_max_memory)}"
             )
         print(f"expected memory usage: {format_mem(expected_memory_usage)}")
 
         print("initialize mcmc state...")
-        # we first put all arguments on the device and only afterwards call init
-        # because init uses the arguments to determine the device we are working
-        # on and automatically configure settings related to performance.
-        kwargs = dict(
-            X=data.quantized_X,
-            y=data.y,
-            offset=0.0,
-            max_split=data.max_split,
-            num_trees=cfg.ntree,
-            p_nonterminal=make_p_nonterminal(cfg.bartz_xgboost_maxdepth, 0.95, 2),
-            leaf_prior_cov_inv=jnp.float32(cfg.ntree),
-            error_cov_df=2.0,
-            error_cov_scale=2.0,
-            min_points_per_decision_node=10 if cfg.n > 10 else None,
-        )
-        key, kwargs = device_put((key, kwargs), cfg.device, donate=True)
+        assert cfg.data_device == cfg.device
         with default_device(cfg.device):
-            # init creates new arrays on the default device, even if it
-            # recognizes the device of inputs and it is different
-            self.state = init(**kwargs)
+            # init creates new arrays on the default device
+            self.state = init(
+                X=data.quantized_X,
+                y=data.y,
+                offset=0.0,
+                max_split=data.max_split,
+                num_trees=cfg.ntree,
+                p_nonterminal=make_p_nonterminal(cfg.bartz_xgboost_maxdepth, 0.95, 2),
+                leaf_prior_cov_inv=jnp.float32(cfg.ntree),
+                error_cov_df=2.0,
+                error_cov_scale=2.0,
+                min_points_per_decision_node=10 if cfg.n > 10 else None,
+            )
+        del data
+        key = device_put(key, cfg.device)
         self.device = cfg.device
 
         print("compile mcmc loop...")
@@ -270,7 +338,7 @@ class Dbarts(Benchmark):
         # decide whether to skip
         expected_memory_usage = 24 * cfg.n * (cfg.ntree + cfg.p)
         if expected_memory_usage > cfg.cpu_max_memory:
-            raise Skip(
+            raise Stop(
                 f"cpu memory limit exceeded: {format_mem(expected_memory_usage)} > {format_mem(cfg.cpu_max_memory)}"
             )
         print(f"expected memory usage: {format_mem(expected_memory_usage)}")
@@ -319,11 +387,11 @@ class Xgboost(Benchmark):
                 cfg.n * cfg.p > cfg.cpu_max_memory // 16
                 or cfg.n * cfg.ntree > max_n_times_ntree
             ):
-                raise Skip("cpu memory or time limit exceeded")
+                raise Stop("cpu memory or time limit exceeded")
         else:  # gpu
             # to avoid out-of-memory session termination
             if cfg.n * cfg.p > cfg.xgboost_gpu_max_n_times_p:
-                raise Skip("gpu memory limit exceeded")
+                raise Stop("gpu memory limit exceeded")
 
         print(f"n * p = {cfg.n * cfg.p:_}, n * ntree = {cfg.n * cfg.ntree:_}")
 
@@ -364,11 +432,14 @@ def benchmark_unit(key: Key[Array, ""], cfg: UnitConfig) -> float:
     key = keys.pop()
 
     print("generate data...")
-    data = make_data(keys.pop(), cfg.n, cfg.p)
+    with default_device(cfg.data_device):
+        data = make_data(keys.pop(), cfg.n, cfg.p, quantized_x=cfg.quantize_x)
+        block_until_ready(data)
 
     # the harness prints its own messages
     bench = cfg.benchclass()
     bench.setup(keys.pop(), data, cfg)
+    del data
 
     print("run...")
     times = []
@@ -402,20 +473,23 @@ def benchmark_loop(config: Config) -> dict[str, list]:
             time_per_iter = benchmark_unit(keys.pop(), config.unit_config(n))
 
         except JaxRuntimeError as exc:
-            # suppress out-of-memory errors, without saving results
             if not exc.args[0].startswith(
                 "RESOURCE_EXHAUSTED: Out of memory while trying to allocate"
             ):
+                # unknown error, don't catch
                 raise
             else:
-                print(f"Skip benchmark unit with out-of-memory error:\n{exc}")
+                # out of memory, stop the loop
+                print(f"\nStop benchmark loop with out-of-memory error:\n{exc}")
+                break
 
-        except Skip as exc:
-            # don't save results
-            print(f"Skip benchmark unit with exception:\n{exc}")
+        except Stop as exc:
+            # stop the loop
+            print(f"\nStop benchmark loop with exception:\n{exc}")
+            break
 
         else:
-            # save results
+            # all normal, save results
             results.setdefault("n", []).append(n)
             results.setdefault("time_per_iter", []).append(time_per_iter)
 
@@ -444,7 +518,7 @@ def save_results(cfg: Config, results: dict[str, list[Any]]) -> None:
         output["p"] = cfg.fixed_p
 
     # determine filename suffix
-    suffix = f"-{output['package']}-{output['device_kind']}"
+    suffix = f"-{output['package']}-{output['device_kind'].replace(' ', '_')}"
     if "n/ntree" in output:
         suffix += "-highntree"
     if "n/p" in output:
@@ -474,7 +548,7 @@ def setup_device(cfg: Config) -> None:
             raise ValueError(cfg.platform)
 
 
-def parse_args() -> Namespace:
+def parse_args(argv: Sequence[str]) -> Namespace:
     """Parse command line arguments."""
     parser = ArgumentParser(
         description=__doc__,
@@ -520,7 +594,7 @@ def parse_args() -> Namespace:
         default="cpu",
         help="device to run the benchmark on",
     )
-    return parser.parse_args()
+    return parser.parse_args(argv)
 
 
 def args_to_config(args: Namespace) -> Config:
@@ -539,9 +613,9 @@ def args_to_config(args: Namespace) -> Config:
     return Config(**cfg_kwargs)
 
 
-def main() -> None:
+def main(argv: Sequence[str] = sys.argv[1:]) -> None:
     """Entry point of the script."""
-    args = parse_args()
+    args = parse_args(argv)
     cfg = args_to_config(args)
     setup_device(cfg)
     results = benchmark_loop(cfg)
