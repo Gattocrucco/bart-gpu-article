fbbfa7d11909f33a17f84e0a7bad86637696af82
Gattocrucco <info@giacomopetrillo.com>
2026-01-28 01:58:25 +0100

    feat: selection of datasets

---
+++ src/bart_gpu_article/scripts/list_datasets.py
@@ -0,0 +1,135 @@
+"""Get a list of OPENML datasets to use."""
+
+import json
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import polars as pl
+from openml.datasets import list_datasets
+
+
+def get_data() -> pd.DataFrame:
+    """Download list of OpenML datasets."""
+    return list_datasets(output_format="dataframe")
+
+
+def filter_data(df: pd.DataFrame) -> pl.DataFrame:
+    """Filter list of datasets."""
+    return (
+        pl.DataFrame(df)
+        .rename(
+            {
+                "NumberOfInstances": "n",
+                "NumberOfFeatures": "p",
+                "NumberOfClasses": "k",
+                "NumberOfNumericFeatures": "pcont",
+                "NumberOfSymbolicFeatures": "pcat",
+            }
+        )
+        .group_by("name")
+        .agg(
+            # keep only the latest version of each dataset
+            pl.all().max_by("version")
+        )
+        .filter(
+            # possibly low-quality synthetic datasets
+            pl.col("name")
+            .is_in(
+                [
+                    "Agrawal1",
+                    "friedman1",
+                    "Census-(Augmented)",
+                    "autos",
+                    "breast",
+                    "chen_10",
+                    "chen_10_null",
+                    "colon",
+                    "prostate",
+                ]
+            )
+            .not_(),
+            pl.col("name").str.starts_with("BNG(").not_(),
+            pl.col("name").str.starts_with("simulated_").not_(),
+            pl.col("name").str.starts_with("Hyperplane_").not_(),
+            pl.col("name").str.starts_with("SEA(").not_(),
+            pl.col("name").str.starts_with("Stagger").not_(),
+            pl.col("name").str.starts_with("bates_").not_(),
+            # end synthetic datasets
+            pl.col("name").ne("click"),  # don't know what this is
+            pl.col("name").ne("subset_higgs"),  # dunno and seems redundant with Higgs
+            pl.col("name").str.starts_with("BAF_variant").not_(),  # a bit redundant
+            pl.col("name").ne("Airlines_DepDelay_1M"),  # redundant with 10M version
+            pl.col("k").eq(0) | pl.col("k").eq(2),  # continuous or binary outcome
+            pl.col("n") >= 1_000_000,  # large sample size
+            NumberOfMissingValues=0,  # no missing values
+        )
+        .with_columns((pl.col("pcont") / pl.col("p")).alias("pcont_over_p"))
+        .sort("name")
+    )
+
+
+def print_data_info(df: pl.DataFrame) -> None:
+    """Print the list of names of the selected datasets."""
+    for row in df.select(["name", "did"]).iter_rows():
+        name, did = row
+        print(f"{did:5}  {name}")
+
+
+def save_ids(df: pl.DataFrame) -> None:
+    """Save list of dataset ids."""
+    ids = df["did"].to_list()
+    out = json.dumps(ids)
+    file = Path("./data") / "list-datasets.json"
+    file.parent.mkdir(parents=True, exist_ok=True)
+    print(f"write {file}...")
+    file.write_text(out)
+
+
+def plot_datasets_metadata(df: pl.DataFrame) -> plt.Figure:
+    """Plot histograms of metadata of datasets."""
+    fig, axes = plt.subplots(
+        2, 3, figsize=(12, 8), layout="constrained", clear=True, num="list-datasets"
+    )
+    axes = axes.flatten()
+
+    # Define variables to plot
+    hist_vars = [
+        "n",
+        "p",
+        "k",
+        "pcont",
+        "pcat",
+        "pcont_over_p",
+    ]
+
+    for ax, col in zip(axes, hist_vars):
+        data = df[col]
+        assert data.null_count() == 0, col
+        ax.hist(data, bins="auto", histtype="stepfilled")
+        ax.set(xlabel=col, ylabel="Count per bin")
+
+    return fig
+
+
+def save_fig(fig: plt.Figure) -> None:
+    """Save a figure."""
+    file = Path("./plots") / f"{fig.get_label()}.pdf"
+    file.parent.mkdir(parents=True, exist_ok=True)
+    print(f"write {file}...")
+    fig.savefig(file)
+
+
+def main() -> None:
+    """Entry point of the script."""
+    df = get_data()
+    df = filter_data(df)
+    print_data_info(df)
+    save_ids(df)
+    fig = plot_datasets_metadata(df)
+    save_fig(fig)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
